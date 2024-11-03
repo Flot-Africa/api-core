d498c7c320f79b0921bfe6b17b4d54f22f22a39c
baazz <yaoeloge@outlook.fr>
2024-11-03 20:14:17 +0000

    fix(package): get lead package by id

---
+++ src/main/java/africa/flot/application/dto/query/PackageDTO.java
@@ -0,0 +1,25 @@
+package africa.flot.application.dto.query;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+public class PackageDTO {
+    private UUID id;
+    private String name;
+    private String description;
+    private BigDecimal weeklyPayment;
+    private Integer contractDuration;
+    private Integer weeklyMileageLimit;
+    private Boolean maintenanceIncluded;
+    private Boolean insuranceIncluded;
+    private BigDecimal excessMileageFee;
+    private Boolean roadsideAssistance;
+    private Boolean chargingAccess;
+    private Boolean purchaseOption;
+    private Boolean rewardProgram;
+
+}
+
